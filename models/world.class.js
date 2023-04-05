9aff4074996581e08d2b8d9cdea02d05318ae85c
Kay <kay@hp>
2023-04-05 19:52:13 +0200

    genereted new objects of enviroment

--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,30 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken(), new Chicken()];
+  enviroment = [
+    new Enviroment("img/5_background/layers/4_clouds/1.png", 0, 0, 720, 440),
+    new Enviroment(
+      "img/5_background/layers/3_third_layer/1.png",
+      0,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/2_second_layer/1.png",
+      0,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/1_first_layer/1.png",
+      0,
+      0,
+      720,
+      480
+    )
+  ];
   canvas;
   ctx;
 
@@ -12,6 +36,11 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.enviroment.forEach((e) => {
+      this.ctx.drawImage(e.img, e.x, e.y, e.width, e.height);
+    });
+
     this.ctx.drawImage(
       this.character.img,
       this.character.x,
