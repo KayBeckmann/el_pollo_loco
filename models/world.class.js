d700cc3366a03ac8c795efae939fa4c9043966bc
Kay <kay@hp>
2023-04-05 06:30:12 +0200

    Clear Canvas and redraw world.

--- models/world.class.js
+++ models/world.class.js
@@ -1,13 +1,17 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken(), new Chicken()];
+  canvas;
   ctx;
 
   constructor(canvas) {
+    this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.draw();
   }
+
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.drawImage(
       this.character.img,
       this.character.x,
@@ -15,5 +19,21 @@ class World {
       this.character.width,
       this.character.height
     );
+
+    this.enemies.forEach((enemie) => {
+      this.ctx.drawImage(
+        enemie.img,
+        enemie.x,
+        enemie.y,
+        enemie.width,
+        enemie.height
+      );
+    });
+
+    let self = this; //'This' kann nicht an Callback direkt übergeben werden
+
+    requestAnimationFrame(function () {
+      self.draw();
+    });
   }
 }
