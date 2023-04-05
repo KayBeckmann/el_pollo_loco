ec8cfde9966a4b549a0cb9971c337be4ab8b5d6a
Kay <kay@hp>
2023-04-05 19:51:28 +0200

    changed start-position and height

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,6 +1,10 @@
 class Chicken extends MovableObject {
+  y = 350;
+  height = 80;
+  width = 80;
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.x = 200 + Math.random() * 500;
   }
   moveLeft() {}
 }
