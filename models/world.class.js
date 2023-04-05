7d577269dcabf1c5aef7483bd953eb6210ecc0bd
Kay <kay@hp>
2023-04-05 20:14:37 +0200

    added comments

--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,9 @@
 class World {
+  //Generate Player
   character = new Character();
+  //Generate Enemies
   enemies = [new Chicken(), new Chicken(), new Chicken(), new Chicken()];
+  //Generate Enviroment
   enviroment = [
     new Enviroment("img/5_background/layers/4_clouds/1.png", 0, 0, 720, 440),
     new Enviroment(
@@ -25,35 +28,52 @@ class World {
       480
     )
   ];
+
   canvas;
   ctx;
 
+  /**
+   * Initialisation
+   * @param {HTML-Element} canvas
+   */
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.draw();
   }
 
+  /**
+   * Draw the world
+   */
   draw() {
+    //Clear Canvas
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    //Draw the Clouds and Background
     this.enviroment.forEach((e) => {
       this.addToMap(e);
     });
 
+    //Draw the Player
     this.addToMap(this.character);
 
+    //Draw the Enemies
     this.enemies.forEach((enemie) => {
       this.addToMap(enemie);
     });
 
     let self = this; //'This' kann nicht an Callback direkt übergeben werden
 
+    //Callback-Recursion damit der PC nicht abstürzt -> Recursion führt zu unzulässiger Endlosschleife
     requestAnimationFrame(function () {
       self.draw();
     });
   }
 
+  /**
+   * Draw the Object on the HTML-Canvas
+   * @param {OBJECT} mo
+   */
   addToMap(mo) {
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
