bf4d2a4cb18cc1f875011f70ff93153f9304f8ae
Kay Beckmann <info@beckmann-md.de>
2023-04-08 15:24:08 +0200

    added air-background

--- models/world.class.js
+++ models/world.class.js
@@ -5,27 +5,40 @@ class World {
   enemies = [new Chicken(), new Chicken(), new Chicken(), new Chicken()];
   //Generate Enviroment
   enviroment = [
-    new Enviroment("img/5_background/layers/4_clouds/1.png", 0, 0, 720, 440),
+    new Enviroment(
+      "img/5_background/layers/air.png",
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    ),
+    new Enviroment(
+      "img/5_background/layers/4_clouds/1.png",
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    ),
     new Enviroment(
       "img/5_background/layers/3_third_layer/1.png",
       0,
       0,
-      720,
-      480
+      canvas.width,
+      canvas.height
     ),
     new Enviroment(
       "img/5_background/layers/2_second_layer/1.png",
       0,
       0,
-      720,
-      480
+      canvas.width,
+      canvas.height
     ),
     new Enviroment(
       "img/5_background/layers/1_first_layer/1.png",
       0,
       0,
-      720,
-      480
+      canvas.width,
+      canvas.height
     )
   ];
 
