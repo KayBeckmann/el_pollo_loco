0be091195ccd87918c042e09aac4d2ee79b34ae0
Kay Beckmann <info@beckmann-md.de>
2023-04-12 21:29:48 +0200

    Random speed for chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -15,6 +15,7 @@ class Chicken extends MovableObject {
     super().loadImages(this.IMAGES_WALKING);
     this.loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.5;
     this.animate();
   }
 }
