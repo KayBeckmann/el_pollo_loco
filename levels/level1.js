62b9098618339ad7bf144c2a616ab81d29d1162f
Kay <info@beckmann-md.de>
2023-04-17 18:57:35 +0200

    Level creation ausgelagert.

---
+++ levels/level1.js
@@ -0,0 +1,109 @@
+//TODO: HEIGHT and WIDTH dynamicly
+const level1 = new Level(
+  [
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new Chicken()
+  ],
+  [
+    new Enviroment("img/5_background/layers/air.png", 0, 0, 720, 480),
+    new Enviroment(
+      "img/5_background/layers/3_third_layer/1.png",
+      0,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/2_second_layer/1.png",
+      0,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/1_first_layer/1.png",
+      0,
+      0,
+      720,
+      480
+    ),
+    new Enviroment("img/5_background/layers/air.png", 719, 0, 720, 480),
+    new Enviroment(
+      "img/5_background/layers/3_third_layer/2.png",
+      719,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/2_second_layer/2.png",
+      719,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/1_first_layer/2.png",
+      719,
+      0,
+      720,
+      480
+    ),
+    new Enviroment("img/5_background/layers/air.png", 719 * 2, 0, 720, 480),
+    new Enviroment(
+      "img/5_background/layers/3_third_layer/1.png",
+      719 * 2,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/2_second_layer/1.png",
+      719 * 2,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/1_first_layer/1.png",
+      719 * 2,
+      0,
+      720,
+      480
+    ),
+    new Enviroment("img/5_background/layers/air.png", 719 * 3, 0, 720, 480),
+    new Enviroment(
+      "img/5_background/layers/3_third_layer/2.png",
+      719 * 3,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/2_second_layer/2.png",
+      719 * 3,
+      0,
+      720,
+      480
+    ),
+    new Enviroment(
+      "img/5_background/layers/1_first_layer/2.png",
+      719 * 3,
+      0,
+      720,
+      480
+    )
+  ],
+  [
+    new Clouds("img/5_background/layers/4_clouds/1.png", 0, 0, 720, 480),
+    new Clouds("img/5_background/layers/4_clouds/2.png", 719, 0, 720, 480),
+    new Clouds("img/5_background/layers/4_clouds/1.png", 719 * 2, 0, 720, 480),
+    new Clouds("img/5_background/layers/4_clouds/2.png", 719 * 3, 0, 720, 480)
+  ]
+);
---
+++ models/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+  enemies;
+  enviroment;
+  clouds;
+
+  constructor(enemies, enviroment, clouds) {
+    this.enemies = enemies;
+    this.enviroment = enviroment;
+    this.clouds = clouds;
+  }
+}
