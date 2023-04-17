8a7c5eb105904161e757cbe2b6a88f5bec4eadde
Kay <info@beckmann-md.de>
2023-04-17 20:16:15 +0200

    added endboss

--- levels/level1.js
+++ levels/level1.js
@@ -8,7 +8,8 @@ const level1 = new Level(
     new Chicken(),
     new Chicken(),
     new Chicken(),
-    new Chicken()
+    new Chicken(),
+    new Endboss()
   ],
   [
     new Enviroment("img/5_background/layers/air.png", 0, 0, 720, 480),
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,1 +1,20 @@
-class Endboss extends MovableObject {}
+class Endboss extends MovableObject {
+  speed = 0.2;
+  y = 95;
+  height = 350;
+  width = 250;
+  IMAGES_WALKING = [
+    "img/4_enemie_boss_chicken/1_walk/G1.png",
+    "img/4_enemie_boss_chicken/1_walk/G2.png",
+    "img/4_enemie_boss_chicken/1_walk/G3.png",
+    "img/4_enemie_boss_chicken/1_walk/G4.png"
+  ];
+
+  constructor() {
+    super().loadImages(this.IMAGES_WALKING);
+    this.loadImage("img/4_enemie_boss_chicken/1_walk/G1.png");
+    this.x = 720 * 4;
+    // this.speed = 0.15 + Math.random() * 0.5;
+    this.animate();
+  }
+}
